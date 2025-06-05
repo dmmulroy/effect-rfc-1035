26de63684777ce1e47f83d04c74226b58a6dbf87
Dillon Mulroy <dillon.mulroy@gmail.com>
2025-06-05 10:02:01 -0400

    decoding question

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { Effect, ParseResult, Schema } from "effect";
+import { Array as Arr, ParseResult, Schema } from "effect";
+import type { Mutable } from "effect/Types";
 
 const Nibble = Schema.Number.pipe(
 	Schema.between(0, 15, {
@@ -15,15 +16,30 @@ const Uint3 = Schema.Number.pipe(
 );
 
 type Bit = typeof Bit.Type;
+
 const Bit = Schema.Literal(0, 1).annotations({
 	identifier: "Bit",
 	description: "a 1-bit unsigned integer",
 });
 
+const Uint8 = Schema.Number.pipe(
+	Schema.between(0, 255, {
+		identifier: "Byte",
+		description: "a 8-bit unsigned integer",
+	}),
+);
+
 const Uint16 = Schema.Number.pipe(
 	Schema.between(0, 65_535, {
 		identifier: "Uint16",
 		description: "a 16-bit unsigned integer",
+	}),
+);
+
+const Int32 = Schema.Number.pipe(
+	Schema.between(-2_147_483_648, 2_147_483_647, {
+		identifier: "Int32",
+		description: "a 32-bit signed integer",
 	}),
 );
 
@@ -37,7 +53,28 @@ const Uint16 = Schema.Number.pipe(
  * names           255 octets or less
  * TTL             positive values of a signed 32 bit number.
  * UDP messages    512 octets or less
+ *
+ * @see https://www.rfc-editor.org/rfc/rfc1035.html#section-2.3.4
  */
+const Labels = Schema.Array(Uint8)
+	.pipe(Schema.maxItems(63))
+	.annotations({ identifier: "Labels", description: "63 octets or less" });
+
+type Name = typeof Name.Type;
+
+const Name = Schema.Array(Schema.Uint8ArrayFromSelf)
+	.pipe(Schema.maxItems(255))
+	.annotations({ identifier: "Name", description: "255 octets or less" });
+
+const Ttl = Int32.pipe(Schema.positive()).annotations({
+	identifier: "Ttl",
+	description: "positive values of a signed 32 bit number",
+});
+
+const UdpMessages = Schema.Array(Uint8).pipe(Schema.maxItems(63)).annotations({
+	identifier: "UdpMessages",
+	description: "512 octets or less",
+});
 
 export interface Message {
 	header: Header;
@@ -176,8 +213,6 @@ export const Header = Schema.Struct({
 
 export interface Header extends Schema.Schema.Type<typeof Header> {}
 
-const _ = ParseResult.flatMap(() => ParseResult.succeed({}));
-
 export const HeaderFromUint8Array = Schema.transformOrFail(
 	Schema.Uint8ArrayFromSelf,
 	Header,
@@ -189,11 +224,15 @@ export const HeaderFromUint8Array = Schema.transformOrFail(
 					new ParseResult.Type(
 						ast,
 						uint8Array,
-						`Header must be 12 bytes, found ${uint8Array.length}`,
+						`Header must be 12 bytes, received ${uint8Array.length}`,
 					),
 				);
 			}
-			const dataView = new DataView(uint8Array.buffer);
+			const dataView = new DataView(
+				uint8Array.buffer,
+				uint8Array.byteOffset,
+				uint8Array.byteLength,
+			);
 
 			const header = Header.make({
 				id: dataView.getUint16(0),
@@ -249,6 +288,141 @@ export const encodeHeader = Schema.encode(HeaderFromUint8Array);
 export const decodeSyncHeader = Schema.decodeSync(HeaderFromUint8Array);
 export const encodeSyncHeader = Schema.encodeSync(HeaderFromUint8Array);
 
+/**   +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * 4.1.2. Question section format
+ *
+ * The question section is used to carry the "question" in most queries,
+ * i.e., the parameters that define what is being asked. The section
+ * contains QDCOUNT (usually 1) entries, each of the following format:
+ *
+ *                               1  1  1  1  1  1
+ * 0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+ * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * |                                               |
+ * /                     QNAME                     /
+ * /                                               /
+ * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * |                     QTYPE                     |
+ * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ * |                     QCLASS                    |
+ * +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+ *
+ * @see https://www.rfc-editor.org/rfc/rfc1035.html#section-4.1.2
+ */
+export const Question = Schema.Struct({
+	/**
+	 * A domain name represented as a sequence of labels, where
+	 * each label consists of a length octet followed by that
+	 * number of octets. The domain name terminates with the
+	 * zero length octet for the null label of the root. Note
+	 * that this field may be an odd number of octets; no
+	 * padding is used.
+	 */
+	qname: Name,
+
+	/**
+	 * A two octet code which specifies the type of the query.
+	 * The values for this field include all codes valid for a
+	 * TYPE field, together with some more general codes which
+	 * can match more than one type of RR.
+	 */
+	qtype: Uint16,
+
+	/**
+	 * A two octet code that specifies the class of the query.
+	 * For example, the QCLASS field is IN for the Internet.
+	 */
+	qclass: Uint16,
+});
+
+export const QuestionFromUint8Array = Schema.transformOrFail(
+	Schema.Uint8ArrayFromSelf,
+	Question,
+	{
+		strict: true,
+		decode(uint8Array, _, ast) {
+			if (uint8Array.length < 5) {
+				return ParseResult.fail(
+					new ParseResult.Type(
+						ast,
+						uint8Array,
+						`Question must have a minimum length of 5 bytes, received ${uint8Array.length}`,
+					),
+				);
+			}
+
+			if (uint8Array.length > 259) {
+				return ParseResult.fail(
+					new ParseResult.Type(
+						ast,
+						uint8Array,
+						`Question must have a maximum length of 259 bytes, received ${uint8Array.length}`,
+					),
+				);
+			}
+			const dataView = new DataView(
+				uint8Array.buffer,
+				uint8Array.byteOffset,
+				uint8Array.byteLength,
+			);
+
+			let qname: Mutable<Name> = [];
+			let offset = 0;
+
+			while (true) {
+				const length = dataView.getUint8(offset);
+				if (length === 0) {
+					offset += 1;
+					break;
+				}
+
+				if (offset + 1 + length > uint8Array.length) {
+					return ParseResult.fail(
+						new ParseResult.Type(
+							ast,
+							uint8Array,
+							`QNAME label overruns buffer at offset ${offset}`,
+						),
+					);
+				}
+
+				const value = uint8Array.subarray(offset + 1, offset + 1 + length);
+				qname.push(value);
+				offset += length + 1;
+			}
+
+			if (offset + 4 > uint8Array.length) {
+				return ParseResult.fail(
+					new ParseResult.Type(
+						ast,
+						uint8Array,
+						`Not enough bytes for QTYPE and QCLASS after QNAME`,
+					),
+				);
+			}
+
+			const question = Question.make({
+				qname,
+				qtype: dataView.getUint16(offset),
+				qclass: dataView.getUint16(offset + 2),
+			});
+
+			return ParseResult.succeed(question);
+		},
+		encode(header, _, ast) {
+			return ParseResult.fail(
+				new ParseResult.Type(ast, header, `not implemented`),
+			);
+		},
+	},
+);
+
+export const decodeQuestion = Schema.decode(QuestionFromUint8Array);
+// export const encodeQuestion = Schema.encode(QuestionFromUint8Array);
+
+export const decodeSyncQuestion = Schema.decodeSync(QuestionFromUint8Array);
+// export const encodeSyncQuestion = Schema.encodeSync(QuestionFromUint8Array);
+
 // Example DNS header (12 bytes) as a Uint8Array
 // Fields: ID, Flags, QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT
 // Let's use: ID=0x1234, QR=0, Opcode=0, AA=0, TC=0, RD=1, RA=0, Z=0, RCODE=0
@@ -268,3 +442,27 @@ const mockDnsHeader = new Uint8Array([
 	0x00,
 	0x00, // ARCOUNT: 0
 ]);
+
+const mockDnsQuestion = new Uint8Array([
+	7, // length 7
+	101,
+	120,
+	97,
+	109,
+	112,
+	108,
+	101, // "example"
+	3, // length 3
+	99,
+	111,
+	109, // "com"
+	0, // root label
+	0,
+	1, // QTYPE: A (1)
+	0,
+	1, // QCLASS: IN (1)
+]);
+
+const result = decodeSyncQuestion(mockDnsQuestion);
+
+console.log({ result });
